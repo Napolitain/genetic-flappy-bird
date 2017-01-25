4bfcb97ed7faa98032b1804e14cfc8c47be88949
Maxime <maxime.napoleon@gmail.com>
2017-01-25 21:11:41 +0100

    1.0.0

---
+++ main.py
@@ -0,0 +1,93 @@
+#! python3
+
+import tkinter
+import random
+import json, binascii, codecs
+
+# initialisation
+window = tkinter.Tk()
+window.resizable(width = False, height = False)
+window.title("Flappy Bird")
+window.geometry("500x500")
+
+background = tkinter.Canvas(window, width = 500, height = 500, background = "#4CC", bd=0, highlightthickness=0)
+background.pack()
+
+birdImg = tkinter.PhotoImage(file="bird5.png")
+birdX = 100
+birdY = 250
+bird = background.create_image(birdX, birdY, image=birdImg) # (x, y, source)
+flyToggle = 0
+
+pipelineX = 500
+pipelineY = 150
+pipelineTop = background.create_rectangle(pipelineX, 0, pipelineX + 70, pipelineY, fill="#7B2", outline="#7B2")
+pipelineBottom = background.create_rectangle(pipelineX, pipelineY + 100, pipelineX + 70, 500, fill="#7B2", outline="#7B2")
+
+try:
+	with open('stats.json', 'r') as f:
+		temp = f.read()
+		temp = codecs.decode(binascii.unhexlify(codecs.encode(temp)))
+		temp = json.loads(temp)
+		iS, bS, nG = 0, temp['bestScore'], temp['numberGames']
+except:
+	iS, bS, nG = 0, 0, 1
+
+# functions
+def restart(event):
+	global birdX, birdY
+	global pipelineX, pipelineY
+	global iS, bS, nG
+	birdX = 100
+	birdY = 250
+	pipelineX = 500
+	pipelineY = 150
+	background.coords(bird, 100, birdY)
+	background.coords(pipelineTop, pipelineX, 0, pipelineX + 70, pipelineY)
+	background.coords(pipelineBottom, pipelineX, pipelineY + 100, pipelineX + 70, 500)
+	motion()
+	if (bS < iS): # update best score
+		bS = iS
+	with open('stats.json', 'w+') as f: # write to stats.json
+		temp = json.dumps({'bestScore':bS, 'numberGames':nG})
+		temp = codecs.decode(binascii.hexlify(codecs.encode(temp)))
+		f.write(temp)
+	print('Reboot: %i\nScore: %i\nBest: %i\n' % (nG, iS, bS))
+	nG += 1 # update number of games
+	iS = 0 # reset instant score
+
+def motion():
+	global birdY, flyToggle
+	global pipelineX, pipelineY
+	global iS
+	if (birdY < 470 and flyToggle <= 0): # gravity effect
+		birdY += 4
+		background.coords(bird, 100, birdY)
+	if (birdY > 30 and flyToggle > 0): # fly
+		birdY -= 4
+		flyToggle -= 1
+		background.coords(bird, 100, birdY)
+	if (pipelineX < -100): # pipelines
+		pipelineX = 500
+		temp = [pipelineY - 160, pipelineY + 160] # must correct random impossible challenges
+		pipelineY = random.randint(0, 350)
+	else:
+		pipelineX -= 5
+	if (birdX < pipelineX and (birdX + 9) >= pipelineX): # increment score
+		iS += 1
+	if ((birdX + 21) >= pipelineX and birdX <= (pipelineX + 70)): # collisions
+		if ((birdY - 17) <= pipelineY or (birdY + 17) >= (pipelineY + 100)):
+			restart(0)
+			return False
+	background.coords(pipelineTop, pipelineX, 0, pipelineX + 70, pipelineY)
+	background.coords(pipelineBottom, pipelineX, pipelineY + 100, pipelineX + 70, 500)
+	window.after(10, motion) # infinite callback / 100 frames per sec
+
+def fly(event): # enable fly anti gravity effect
+	global flyToggle
+	flyToggle = 10
+
+# run
+window.after(10, motion)
+window.bind("<space>", fly)
+window.mainloop()
