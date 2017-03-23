17019081d7cf11922fb6d9e33d6ac7879c8bf57b
Sylvain <sylvainreynaud@outlook.com>
2017-03-23 10:23:03 +0000

    Correction des bugs de version

--- genetique.py
+++ genetique.py
@@ -1,6 +1,5 @@
 import random
 
-
 TAUX_MUTATION = 0.1
 POPULATION = 10
 HAUTEUR_MIN = 0
@@ -8,21 +7,11 @@ HAUTEUR_MAX = 500
 DISTANCE_MIN = 0
 DISTANCE_MAX = 300
 
+
 class Genome:
 	def __init__(self, h, d):
-		self.hauteur = h
-		self.distance = d
-		self.score = 0
-
-	def setdistance(self, d): # definit la node distance
-		self.distance = d
-
-	def sethauteur(self, h): # definit la node hauteur
-		self.hauteur = h
-
-	def setscore(self, score): # definit le score
-		self.nodeHauteur = h
-		self.nodeDistance = d
+		self.hauteurNode = h
+		self.distanceNode = d
 		self.distanceActu = 0 # distance actuelle de l'oiseau sur le plan du jeu
 		self.hauteurActu = 0 # hauteur actuelle de l'oiseau sur le plan du jeu
 		self.score = 0 # Score maximum du génome
@@ -33,33 +22,37 @@ class Genome:
 	def sethauteurnode(self, h): # definit la node hauteur
 		self.hauteurNode = h
 
-	def setscore(self, score): # definit le score
+	def setdistanceactu(self, d): # definit la node distance
+		self.distanceActu = d
+
+	def sethauteuractu(self, h): # definit la node hauteur
+		self.hauteurActu = h
+
+	def setscore(self, score): # definit le score 
 		self.score = score
 
 	def mutation(self): # Retourne le genome mute
 		return Genome(self.h+random.uniform(HAUTEUR_MIN*TAUX_MUTATION, HAUTEUR_MAX*TAUX_MUTATION),\
 		 self.d+random.uniform(DISTANCE_MIN*TAUX_MUTATION, DISTANCE_MAX*TAUX_MUTATION))
 
-	def getdistance(self): # Retourne la distance
-		return self.distance
-
-	def gethauteur(self): # Retourne la distance
-		return self.hauteur
-
 	def getdistancenode(self): # Retourne la distance
-		return self.distance
+		return self.distanceNode
 
 	def gethauteurnode(self): # Retourne la distance
-		return self.hauteur
+		return self.hauteurNode
 
-	def getdistanceactu(self, d): # Retourne la distance actuelle
+	def getdistanceactu(self): # Retourne la distance actuelle
 		return self.distanceActu
 
-	def gethauteuractu(self, h): # Retourne la node hauteur actuelle
+	def gethauteuractu(self): # Retourne la node hauteur actuelle
 		return self.hauteurActu
 
 	def showinfo(self): # Afficher des informations sur le genome
-		print("Hauteur : ", self.hauteur, "\nDistance : ", self.distance, "\nScore :", self.score)
+		print("hauteurNode : ", self.hauteurNode,\
+		 "\ndistanceNode : ", self.distanceNode,\
+		 "\nHauteur actuelle : ", self.hauteurActu, \
+		 "\nDistance actuelle : ", self.distanceActu, \
+		  "\nScore :", self.score)
 
 
 def randomHauteur(): # Donne une hauteur aleatoire
@@ -67,6 +60,7 @@ def randomHauteur(): # Donne une hauteur aleatoire
 
 def randomDistance(): # Donne une distance aleatoire
 	return random.uniform(DISTANCE_MIN, DISTANCE_MAX);
+	
 
 # Generation d'un genome
 def genGenome(bestG = 0):
@@ -74,3 +68,21 @@ def genGenome(bestG = 0):
 		return Genome(randomHauteur(), randomDistance())
 	else:
 		return bestG.mutation()
+
+'''
+### TESTS ###
+
+g1 = genGenome()
+g1.showinfo()
+
+g1.sethauteuractu(10)
+g1.sethauteurnode(23)
+g1.setdistancenode(39)
+g1.setdistanceactu(21)
+
+print(g1.gethauteuractu())
+print(g1.getdistanceactu())
+print(g1.gethauteurnode())
+print(g1.getdistancenode())
+
+'''
