c02e3bdcc9946c397da51358cc4b536f4a138644
Maxime <maxime.napoleon@gmail.com>
2017-03-23 11:24:20 +0000

    Update

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ flyToggle = 0
 pipelineX = 500
 pipelineY = 150
 pipelineTop = background.create_rectangle(pipelineX, 0, pipelineX + 70, pipelineY, fill="#7B2", outline="#7B2")
-pipelineBottom = background.create_rectangle(pipelineX, pipelineY + 100, pipelineX + 70, 500, fill="#7B2", outline="#7B2")
+pipelineBottom = background.create_rectangle(pipelineX, pipelineY + 125, pipelineX + 70, 500, fill="#7B2", outline="#7B2")
 
 try:
 	with open('stats.json', 'r') as f:
@@ -36,10 +36,10 @@ try:
 except:
 	iS, bS, nG = 0, 0, 1
 
-def getBirdY():
-	return birdY-(pipelineY+100)
+def getBirdY(): # hauteur de l'oiseau relative au pipeline
+	return pipelineY+125 - birdY
 
-def getBirdX():
+def getBirdX(): # distance de l'oiseau relative au pipeline
 	return pipelineX-birdX
 
 iSText = background.create_text(230, 50, fill="white", font="Times 50 bold", text="0") # texte score actuel
@@ -67,8 +67,8 @@ def restart(event):
 		temp = json.dumps({'bestScore':bS, 'numberGames':nG})
 		temp = codecs.decode(base64.b85encode(codecs.encode(temp)))
 		f.write(temp)
-	print('Reboot: %i\nScore: %i\nbest: %i\n' % (nG, iS, bS))
-	nG += 1 # incrémente le nombre de parties
+	# print('Reboot: %i\nScore: %i\nbest: %i\n' % (nG, iS, bS))
+	nG += 1 # incremente le nombre de parties
 	background.itemconfig(nGText, text="n: " + str(nG))
 	iS = 0 # reset instant score
 	background.itemconfig(iSText, text=iS)
@@ -77,13 +77,13 @@ def motion(): # fonction principale
 	global birdY, flyToggle
 	global pipelineX, pipelineY
 	global iS
-	if (birdY < 470 and flyToggle <= 0): # effet de gravité
+	if (birdY < 470 and flyToggle <= 0): # effet de gravite
 		birdY += 4
 		background.coords(bird, 100, birdY)
 		background.itemconfig(bSText, text="best: " + str(bS))
 		background.itemconfig(heightText, text="h: " + str(getBirdY()))
 	if (birdY > 30 and flyToggle > 0): # vole
-		birdY -= 4
+		birdY -= 4.5
 		flyToggle -= 1
 		background.coords(bird, 100, birdY)
 		background.itemconfig(bSText, text="best: " + str(bS))
@@ -93,7 +93,7 @@ def motion(): # fonction principale
 		pipelineY = random.randint(max(pipelineY - 160, 0), min(pipelineY + 160, 350)) # (0, 350) avant, maintenant cas impossibles bannis
 	else:
 		pipelineX -= 5
-	if (birdX < pipelineX and (birdX + 9) >= pipelineX): # incrémente le score
+	if (birdX < pipelineX and (birdX + 9) >= pipelineX): # incremente le score
 		iS += 1
 		background.itemconfig(iSText, text=iS)
 	if ((birdX + 21) >= pipelineX and birdX <= (pipelineX + 70)): # collisions
@@ -103,11 +103,11 @@ def motion(): # fonction principale
 	background.itemconfig(distanceText, text="d: " + str(getBirdX()))
 	background.coords(pipelineTop, pipelineX, 0, pipelineX + 70, pipelineY)
 	background.coords(pipelineBottom, pipelineX, pipelineY + 100, pipelineX + 70, 500)
-	window.after(10, motion) # boucle infinie, 100 images par secondes
+	window.after(15, motion) # boucle infinie, 100 images par secondes
 
 def fly(event): # active l'action de voler
 	global flyToggle
-	flyToggle = 10
+	flyToggle = 15
 
 # run
 window.after(10, motion)
